960ffb11be8f70456de6b773b467c1374bc272f3
seockhun <hun00719@gmail.com>
2021-04-30 11:44:17 +0900

    [ADD] TS PR FILE STRUCTURE
    .

---
+++ typescript-pr/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  children?: React.ReactNode;
+  width: number;
+  onClick?: () => void;
+}
+
+const App: React.FC<Props> = (props) => {
+  const { width, children, onClick } = porps;
+}
