f172375bea573cfb4fad70b24d3849d67d0c4887
seockhun <hun00719@gmail.com>
2021-05-12 19:42:15 +0900

    [TIL] CHANGE SCROLL ANIMATION
    .

--- animation-ex/src/App.js
+++ animation-ex/src/App.js
@@ -1,10 +1,50 @@
 import React from "react";
-import Reveal from "./components/Reveal";
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, Move, MoveIn, MoveOut, Sticky, StickyIn, ZoomIn } from "react-scroll-motion";
 
 const App = () => {
+  const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+  const FadeUp = batch(Fade(), Move(), Sticky());
+
   return (
-    <Reveal />
-  )
-}
+    <ScrollContainer>
+      <ScrollPage page={0}>
+        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
+          <span style={{ fontSize: "3em", textAlign: "center" }}>
+            Let't me show you scroll animation
+            <p style={{ margin: "0" }}>😀</p>
+          </span>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={1}>
+        <Animator animation={ZoomInScrollOut}>
+          <span style={{ fontSize: "3em" }}>I'm FadeUpScrollOut ✨</span>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={2}>
+        <Animator animation={FadeUp}>
+          <span style={{ fontSize: "3em" }}>I'm FadeUp ⛅️</span>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={3}>
+        <div style={{ justifyContent: "center", alignItems: "center", display: "flex" }}>
+          <span style={{ fontSize: "3em" }}>
+            <Animator animation={MoveIn(-1000, 0)}>Hello Guys 👋🏻</Animator>
+            <Animator animation={MoveIn(1000, 0)}>Nice to meet you 🙋🏻‍♀️</Animator>- I'm asdasd
+            <Animator animation={MoveOut(1000, 0)}>Good bye ✋🏻</Animator>
+            <Animator animation={MoveOut(-1000, 0)}>See you 💛</Animator>
+          </span>
+        </div>
+      </ScrollPage>
+      <ScrollPage page={4}>
+        <Animator animation={batch(Fade(), Sticky())}>
+          <p style={{ fontSize: "3em", margin: "0" }}>Done</p>
+          <span style={{ fontSize: "3em" }}>
+            There's FadeAnimation, MoveAnimation, StickyAnimation, ZoomAnimation
+          </span>
+        </Animator>
+      </ScrollPage>
+    </ScrollContainer>
+  );
+};
 
 export default App;
