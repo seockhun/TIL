2be676fb10ae546eda17e19c70d31bfe1b4b278f
seockhun <hun00719@gmail.com>
2021-07-06 08:22:16 +0900

    [ADD] basic file structure

---
+++ redux-practice/src/components/Main/Main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Main = () => {
+    return (
+
+    )
+}
+
+const RenderProducts = (e: any) => {
+    return e.products.map((product) => {
+        return (
+            <div></div>
+        )
+    })
+}
+
+export default Main;
